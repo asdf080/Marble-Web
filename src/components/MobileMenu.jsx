8fd85fd72089954eb738531effc2e03e5c0b082e
LDH <ai04120@naver.com>
2024-02-06 12:31:59 +0900

    menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { CgMenuCake } from "react-icons/cg";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function MobileMenu() {
+  const [mobileOpen, setmobileOpen] = useState(false);
+
   return (
     <div className="block md:hidden">
-      <div className="md:hidden text-3xl px-3 cursor-pointer">
+      {/* 아이콘 */}
+      <div onClick={() => setmobileOpen(!mobileOpen)} className="md:hidden text-3xl px-3 cursor-pointer">
         <CgMenuCake />
       </div>
-      MobileMenu
+      {mobileOpen && <div className="z-50 fixed left-0 top-0 bottom-0 flex w-full h-screen bg-main-dark/95 backdrop-blur-lg">asdf</div>}
     </div>
   );
 }
